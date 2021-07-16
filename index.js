9021efe7b75813081ce0ed08e1cb7dd16480683c
Mark Caldwell <caldweme@yahoo.com>
2021-07-16 20:55:11 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length += 1;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+       return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+       return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    let result = 0;
+      for (let i = 0; i < this.items.length; i++) {
+        result += this.items[i];
+      }
+      return result;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+    let result = 0;
+      for (let i = 0; i < this.items.length; i++) {
+        result += this.items[i] / this.items.length;
+      }
+      return result;
+  }
+  }
+
 }
 
 module.exports = SortedList;
